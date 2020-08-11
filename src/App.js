fc7f581644225ce549c6fc952876f434dd652d25
ykavadap <ykavadap@emich.edu>
2020-08-10 20:14:55 -0400

    push code

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { Cards } from "./components";
+
+import styles from "./App.module.css";
+import { fetchData } from "./api";
+
+class App extends React.Component {
+  state = {
+    data: {},
+  };
+
+  async componentDidMount() {
+    const fetchedData = await fetchData();
+
+    this.setState({ data: fetchedData });
+  }
+  render() {
+    const { data } = this.state;
+
+    return (
+      <div className={styles.container}>
+        <h1>COVID-19 TRACKER</h1>
+        <Cards data={data} />
+      </div>
+    );
+  }
 }
 
 export default App;
